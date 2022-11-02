51118e3fd80fc647b0e9997e2719e81452f37ff4
Tiomasik <a.masliy@ukr.net>
2022-11-03 00:28:36 +0200

    change 3d task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,7 +4,7 @@ const refs = {
   inputDelay: document.querySelector('input[name="delay"]'),
   inputStep: document.querySelector('input[name="step"]'),
   inputAmount: document.querySelector('input[name="amount"]'),
-  formEl: document.querySelector('form')
+  formEl: document.querySelector('.form')
 };
 
 const dataPromise = {
@@ -18,40 +18,28 @@ const dataPromise = {
 refs.inputDelay.addEventListener('input', onInputDelay);
 refs.inputStep.addEventListener('input', onInputStep);
 refs.inputAmount.addEventListener('input', onInputAmount);
-refs.formEl.addEventListener('submit', ((evt) => {
+refs.formEl.addEventListener('submit', onStartMesenger);
+
+function onStartMesenger(evt) {
   evt.preventDefault()
+
   for (let index = 0; index < dataPromise.amount; index += 1) {
-    console.log(dataPromise.position += 1)
+    dataPromise.position += 1
     if (Number(dataPromise.position) === 1) {
     dataPromise.delay += Number(dataPromise.firstDelay);
   } else {
     dataPromise.delay += Number(dataPromise.step);
   }
-  
-  console.log(dataPromise.delay)
-    makePromise(dataPromise).then(onMakePromiseSuccess).catch(onMakePromiseError);
-    
+    createPromise(dataPromise).then(onMakePromiseSuccess).catch(onMakePromiseError);
   }
+
   refs.formEl.reset();
   dataPromise.position = 0;
   dataPromise.delay = 0;
-}));
-
-// makePromise(dataPromise) .then(onMakePromiseSuccess).catch(onMakePromiseError);
-
-function onMakePromiseSuccess(result) {
-  Notiflix.Notify.success(result)
-  console.log(result)
 }
 
-function onMakePromiseError(error) {
-  Notiflix.Notify.failure(error);
-  console.log(error)
-}
-
-function makePromise({position, delay}) {
-
-  const shouldResolve = Math.random() > 0.5;
+function createPromise({position, delay}) {
+  const shouldResolve = Math.random() > 0.3;
  
   return new Promise((resolve, reject) => {
 
@@ -62,40 +50,15 @@ function makePromise({position, delay}) {
         reject(`Rejected promise ${position} in ${delay}ms`)
       }
     }, delay)
-      //   if (amount > position) {
-  //     setTimeout((() => {
-  //       console.log(position += 1);
-  //       delay += Number(firstDelay);
-  //       const shouldResolve = Math.random() > 0.5;
-  //       if (shouldResolve) {
-  //       resolve(`Fulfilled promise ${position} in ${delay}ms`)
-  //       } else {
-  //       reject(`Rejected promise ${position} in ${delay}ms`);
-  //     }
-      
-      
-  //       if (amount > position) {
-  //         const intervalPromise = setInterval((() => {
-  //           console.log(position += 1);
-  //           delay += Number(step);
-  //           const shouldResolve = Math.random() > 0.5;
-  //       if (shouldResolve) {
-  //       resolve(`Fulfilled promise ${position} in ${delay}ms`)
-  //       } else {
-  //       reject(`Rejected promise ${position} in ${delay}ms`);
-  //     }
-            
-            
-  //           if (amount <= position) {
-  //             clearInterval(intervalPromise);
-  //             console.log('Stop!!!');
-  //           }
-  //         }), step)
-  //       }
-  //     // dataPromise.position = 0;
-  //   }), firstDelay)
-  // }
   })
+}
+
+function onMakePromiseSuccess(result) {
+  Notiflix.Notify.success(result)
+}
+
+function onMakePromiseError(error) {
+  Notiflix.Notify.failure(error);
 }
 
 function onInputDelay(evt) {
@@ -110,11 +73,3 @@ function onInputAmount(evt) {
   dataPromise[`amount`] = `${evt.target.value}`;
 }
 
-// function createPromise({ position, delay }) {
-//   const shouldResolve = Math.random() > 0.5;
-//   if (shouldResolve) {
-//     resolve(`Fulfilled promise ${position} in ${delay}ms`)
-//   } else {
-//     reject(`Fulfilled promise ${position} in ${delay}ms`);
-//   }
-// }
